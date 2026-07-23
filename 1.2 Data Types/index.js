d190cce207f175c42e911d7a5098e596be7b4fd9
aniilroy <anilroy.cce@gmail.com>
2026-07-24 02:08:31 +0530

    data types

---
+++ 1.2 Data Types/index.js
@@ -0,0 +1,30 @@
+
+//JavaScript is dynamic typing, don't need to declare its type
+
+// 1. Number data type, its mainly floating point and integers numbers
+
+let age = 28;
+console.log(typeof(age));
+
+// 2. String data type, its mainly sequence of charactors and used for text
+
+let firstName = 'Anil';
+console.log(typeof(firstName));
+
+// 3. Boolean data type, logical types that can only be true or false and used for taking decisions
+
+let isTrue = true;
+console.log(typeof(isTrue));
+
+// 4. Undefined data, empty value
+
+let peoples;
+
+// 5. Null, Also means "Empty"
+
+// 6. Symbol, unique value that can not be changed
+
+// 7. BigInt, Larger integers
+
+
+
