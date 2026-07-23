65db2c20c0099cdd2f5412e5fa94f1261f163b77
aniilroy <anilroy.cce@gmail.com>
2026-07-24 01:28:27 +0530

    Variables and values

---
+++ 1.1 Variables and values/index.js
@@ -0,0 +1,24 @@
+let js = "amazing";
+console.log(45 + 40 + 213 -50);
+console.log("Anil Roy");
+console.log(57);
+
+//Variable name should be in Camel-Case in JavaScript
+//Invalid variable name 3name, my$name, my name, any keyword, Myname, anil_roy, $function etc.
+
+let firstName = "Anil";
+let lastName = " Roy";
+let name = firstName + lastName;
+console.log(name);
+
+//Good practice for variable name
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+//Bad practice
+
+let job1 = 'Programeer';
+let job2 = 'teacher'
+
+console.log(myFirstJob);
+console.log(myCurrentJob);
