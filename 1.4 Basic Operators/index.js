4b7264a7b27a821883bacb2d6375f138bfc3fd52
aniilroy <anilroy.cce@gmail.com>
2026-07-25 01:55:49 +0530

    Operators in JS

---
+++ 1.4 Basic Operators/index.js
@@ -0,0 +1,40 @@
+//basic operators
+const ageAnil = 2027 - 1992;
+console.log(ageAnil);
+const ageAmit = 2030 - 1972;
+const agePriya = 2024 -2000;
+console.log(ageAmit, agePriya);
+
+const yearNow = 2028;
+const ageShreya = yearNow - 2000;
+const ageMisty = yearNow - 2005;
+console.log(ageShreya , ageMisty);
+console.log(agePriya * 2, ageShreya / 2, 2 ** 3);
+//2 ** 3 means 2 to the power 3 => 2 * 2 * 2
+
+const firstName = 'Anil';
+const lastName = 'Roy';
+console.log(firstName + ' ' + lastName); //concatenation
+
+let x = 10 + 5; //assignment operator
+x += 10; //augmented or shorthand operator, x = x +10;
+x *= 2;
+x -= 20;
+x /= 6
+x++; //increasing by 1
+x--; //decreasing by 1
+console.log(x);
+
+//comparison operator
+
+console.log(ageShreya < ageAnil); // >, < , >=, <=
+console.log(agePriya >=18);
+const isAudalt = agePriya >= 18;
+console.log(isAudalt);
+
+console.log(yearNow - 1999 > yearNow - 1964);
+
+
+
+
+
