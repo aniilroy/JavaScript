b6dfba36d1c8f334c62a584f34c1923657ed6bd9
aniilroy <anilroy.cce@gmail.com>
2026-07-25 00:56:51 +0530

    let, const and var

---
+++ 1.3 let, const and var/index.js
@@ -0,0 +1,21 @@
+//let, const and var
+
+let age = 30; //let is mutable type, value changed
+age = 45;
+
+let number; // no need to initialize
+number = 12; 
+
+const birthYear = 1972; //immutable, value not changed
+//birthYear = 1990; //it show error
+
+//const salary; It show error, It should be initialize
+
+var myJob = 'Doctor'; //similar to let and it old method
+myJob = 'Teacher';
+
+lastName = 'Roy'; //should not write this without declaring but it work
+console.log(lastName);
+
+
+
