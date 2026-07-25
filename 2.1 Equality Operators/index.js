900895d853232ee711a41492a20d55c45c957804
aniilroy <anilroy.cce@gmail.com>
2026-07-25 23:41:09 +0530

    equality operators

---
+++ 2.1 Equality Operators/index.js
@@ -0,0 +1,18 @@
+// equality operator
+
+// Strict comparision ===
+const age = 21; // assignmnet operator: =
+if (age === 21) console.log("Your are an adult now!");
+
+//loose comparision operator ==
+console.log(21 == '21'); //true
+
+  const favNumber = prompt('What is your favourite number?');
+
+console.log(favNumber);
+console.log(typeof favNumber);
+
+if (favNumber == 12){
+    console.log("Nice , 12 is an amazing number!");
+}else ("Try a new number");
+
