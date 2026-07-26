174985ac294329804d20bc2d4549f67abdb75d19
aniilroy <anilroy.cce@gmail.com>
2026-07-26 12:03:42 +0530

    Expressions and statements

---
+++ 2.4 Statements and Expressions/index.js
@@ -0,0 +1,23 @@
+//expression
+10 + 20
+
+60 / 4
+
+2029 - 1971
+
+true && false
+
+false || true
+
+false || !false
+
+const me = 'anil';
+
+console.log(`I am ${2028-1998} years old ${me}.`);
+
+//statements
+let str; 
+if (50 === 50){
+    str = 'they are equal';
+}
+console.log(str);
