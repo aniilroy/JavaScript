2637fb5fd9eae432c2bb16e1a12b75b955fce32b
aniilroy <anilroy.cce@gmail.com>
2026-07-27 00:03:31 +0530

    Introduction to objects

---
+++ 3.1 Intro To Objects/index.js
@@ -0,0 +1,22 @@
+// Introduction of Objects
+
+//This is an Array for comparing
+const myArray = [
+
+    'Anil',
+    'Roy',
+    2030-1992,
+    'programmer',
+    ['Mukul', 'Ram', 'Sham']
+];
+
+//This is an Object with key value pairs, this objects has five properties
+const myObject = {
+    firstName : 'Anil',
+    lastName : 'Roy',
+    age : 2030 - 1992,
+    job : 'programmer',
+    friends : ['Mukul', 'Ram', 'Sham']
+};
+console.log(myArray?.constructor?.name);
+console.log(myObject?.constructor?.name);
