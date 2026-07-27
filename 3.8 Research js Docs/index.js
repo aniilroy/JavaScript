e193c2584fc59a743f433b043daa51bb001933d3
aniilroy <anilroy.cce@gmail.com>
2026-07-28 00:14:50 +0530

    Add concate two array

--- 3.8 Research js Docs/index.js
+++ 3.8 Research js Docs/index.js
@@ -1,5 +1,8 @@
 //research the js documets from mdn, google and stackoverflow
 
+// Make a smart home thermometer. 
+//Task: Given an array of temperatures of one day, calculate the temperature amplitude. Keep in mind that sometimes there might be a sensor error
+
 const temperatures = [3, -5, -7, -2, -6, 'error', 5, 12, 13, 19, 17, 9, 11, 5, 10];
 
 const calTempAmplitude = function (temps){
@@ -19,4 +22,10 @@ const calTempAmplitude = function (temps){
 
 const amplitude = calTempAmplitude (temperatures);
 
-console.log(amplitude)
+console.log(amplitude);
+
+// Now concate two arrays
+ const fistArray = [10, 1, 5, 3 , 9, 2];
+ const secondArray = ['cat', 'dog', 'rabbit', 'fish'];
+ const mergeArray = fistArray.concat(secondArray);
+ console.log(mergeArray);
