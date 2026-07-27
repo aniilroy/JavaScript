a744b83ab45ba36192398a67c46b4aef5fbb3cd2
aniilroy <anilroy.cce@gmail.com>
2026-07-27 10:31:45 +0530

    for loops learned

---
+++ 3.4 Iteration: For Loop/index.js
@@ -0,0 +1,12 @@
+//iteration -- for loop control structure
+console.log('Up and Down, time 1');
+console.log('Up and Down, time 2');
+console.log('Up and Down, time 3');
+console.log('Up and Down, time 4');
+console.log('Up and Down, time 5');
+
+//print same thing 10 times
+for (let i = 1; i <= 10; i++){
+    console.log(`Up and down ${i} 📈 `);
+};
+
